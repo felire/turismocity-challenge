533afcdd5a5e63eb47a3b5ced25f16e6ec977352
Felipe Rodriguez Esturo <felipe.rodriguez@wolox.com.ar>
2020-04-25 17:46:21 -0300

    very little fix on dependencies and redux action name

--- src/redux/images/actions.js
+++ src/redux/images/actions.js
@@ -2,7 +2,7 @@ import { createTypes, completeTypes } from 'redux-recompose';
 import * as ImagesService from '@services/ImagesService';
 import { formatPagination, paginationServicePayload } from '@utils/reduxUtils';
 
-export const actions = createTypes(completeTypes(['GET_IMAGES', 'GET_IMAGE']), '@@AUTH');
+export const actions = createTypes(completeTypes(['GET_IMAGES', 'GET_IMAGE']), '@@IMAGES');
 
 const imagesTarget = 'images';
 const currentImage = 'currentImage';
